d34e2a4afb7c348ea6645bc6078943a9886240e8
Adam Eliezerov <adaminspaceship@gmail.com>
2017-02-14 19:26:08 +0200

    Working with alarm

--- main.py
+++ main.py
@@ -1,60 +1,107 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import requests
+
 import os
-from urllib2 import urlopen
-from bs4 import BeautifulSoup
-import re
-import smtplib
-import ifttt_maker
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import time
+import datetime
+import sys
+import calendar
 
-ifttt = ifttt_maker.Ifttt("blich", "pYTi1N5f7ucT6tqt0PZjDVfgTjg_0rz6wRlI5EorafI")
-gmail_user = 'adam.eliezerov@gmail.com'  
-gmail_password = 'adamadam123'
-fromaddr = 'adam.eliezerov@gmail.com'
-toaddrs  = 'adam.eliezerov@gmail.com'
+classnum = "3"
 
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.starttls()
-server.login(gmail_user,gmail_password)
+#######################################################################################################################################
 
 
-url = "http://blich.iscool.co.il/DesktopModules/IS.TimeTable/MainScreen.aspx?pid=17&mid=6264&page=1&msgof=0&static=1"
 
-r = requests.get(url)
+date = str(datetime.datetime.today().strftime('%w'))
 
-html_content = r.text
+nownownow = datetime.datetime.now()
 
-soup = BeautifulSoup(html_content,"html.parser")
-soup.original_encoding
-'WINDOWS-1255'
+dateweek =  time.strftime("%A")
 
-table = soup("td", {'class' : 'DisplayFreeChange' })
-messagenot = "Not today motherfucka!"
+pathday = '/Users/adam/Desktop/logs/'+date+'.log'
 
-#testing if test found in table0
-try:
-    table0 = table[1]
-    
-except:
-    print "No free lesson found"
-    server.sendmail(fromaddr, toaddrs, messagenot)
-else:
-    print "Free lesson found!"
-    table0str = str(table0)
-    table0strdone = re.sub('<[^>]*>', '', table0str)
-    subject = "ביטול במערכת שעות"
-    message = 'Subject: {}\n\n{}'.format(subject, table0strdone)
-    server.sendmail(fromaddr, toaddrs, message)
-    print (table0strdone)
-    ifttt.trigger(value1=table0strdone)
+timecurrent = time.strftime("%X")
+
+daytoday = open(pathday, 'w')
+
+hournow = ("%s" %nownownow.hour)
+hournowint = int(hournow)
+
+chromedriver = "/Users/adam/Downloads/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chromedriver)
+driver.get("http://blich.iscool.co.il/tabid/2117/language/he-IL/Default.aspx")
+elem = driver.find_element_by_xpath("""//*[@id="dnn_ctr7919_TimeTableView_TdClassesList"]""")
+# value 3 at the end = ט - 3
+ddd = driver.find_element_by_xpath('''//*[@id="dnn_ctr7919_TimeTableView_ClassesList"]/option[@value='2']''').click()
+driver.find_element_by_xpath('''//*[@id="dnn_ctr7919_TimeTableView_btnChangesTable"]''').click()
+
+
+#######################################################################################################################################
+print "Today is " + dateweek
+
+def FreeLesson():
+	T00 =  ''' //*[@class='TTTable']/tbody/tr[4]/td[@class='TTCell'][3]//*[@class='TableExamChange']'''
 	
+	try:
+		driver.find_element_by_xpath(T00)
+	except:
+		return False
+		print "false"
+		
+	else:
+		return True
+		print "true"
+		
 
+FreeLesson()
+#######################################################################################################################################
 
+#alarm function
+def alarm():
+	hour1 = input("what hour do you want to set the alarm to?   ")
+	minute1 = input("what minute do you want to set the alarm to?   ")
+	print('Alarm running...')
+	not_executed = 1
 
-
-
+	while(not_executed == 1):
+		dt = list(time.localtime())
+		hour = dt[3]
+		minute = dt[4]
+		time.sleep(5)
+		if (hour == hour1) and (minute == minute1) and (FreeLesson() == True):
+			hour2 = hour1 + 1
+			minute2 = minute1 + 1
+			hour2str = str(hour2)
+			print "Free Lesson Found... Waking up at " + hour2str
+			not_executed = 3
+			while(not_executed == 3):
+				dt = list(time.localtime())
+				hour = dt[3]
+				minute = dt[4]
+				time.sleep(5)
+				if (hour == hour1) and (minute == minute2):
+					print "Wake Up!"
+					not_executed = 0
+			
+		elif (hour == hour1) and (minute == minute1) and (FreeLesson() == False):
+			print "Wake up, first hour!"
+		
 	
-	
-#end all proccess running
-server.quit()
-	
+			
+
+# end alarm function
+
+
+alarm()
+
+
+
+#quiting all
+
+daytoday.close()
+
+driver.quit()
